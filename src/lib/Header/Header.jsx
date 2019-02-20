5b5ebe39c4bb9861f737c860e6f82dc537200612
christopher cahill and quinn miller <christopher+quinn@>
2019-02-20 15:48:34 -0800

    adds fashionable styling

--- src/lib/Header/Header.jsx
+++ src/lib/Header/Header.jsx
@@ -5,7 +5,11 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div style={{
+        padding: '20px',
+        margin: '20px',
+        borderStyle: 'solid',
+        }}>
 
         <h1>header</h1>
         <h4><Link to='/'>Home Page</Link></h4>
--- src/lib/TamagotchiController/NewTamagotchi.jsx
+++ src/lib/TamagotchiController/NewTamagotchi.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import { v4 } from 'uuid';
-import Tamagotchi from './Tamagotchi/Tamagotchi'
+import Tamagotchi from './Tamagotchi/Tamagotchi';
+import PropTypes from 'prop-types';
+import Moment from 'moment';
 
-class NewTamagotchi extends Component {
-  render() {
+
+
+function NewTamagotchi(props) {
+  let _name = null;
+
+  function handleNewTamagotchiFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTamagotchiCreation({name: _name.value, id: v4()});
+    _name.value = '';
+  }
+
     return (
-      <div>
-        <p>
-          Individual tamagotchi Component
-        </p>
+      <div style = {{
+        padding: '20px',
+        margin: '20px',
+        borderStyle: 'solid',
+        }}>
+        <form onSubmit={handleNewTamagotchiFormSubmission}>
+          <input
+            type='text'
+            id='name'
+            placeholder='Tama name'
+            ref={(input) => {_name = input;}}/>
+          <br></br>
+
+          <button type='submit'>Leave your message!</button>
+        </form>
       </div>
     );
-  }
+
 }
+
+NewTamagotchi.propTypes = {
+  onNewTamagotchiCreation: PropTypes.func
+}
+
 export default NewTamagotchi;
